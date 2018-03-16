5117a744cd22722b963bb5c74c351ea1dcc0cfff
mantas.urbonas <mantas.urbonas@gmail.com>
2018-03-16 17:57:55 +0200

    performance improvements

--- src/main/java/lt/visma/ai/welcomebot/MainFrame.java
+++ src/main/java/lt/visma/ai/welcomebot/MainFrame.java
@@ -32,8 +32,12 @@ import org.bytedeco.javacpp.opencv_face.FaceRecognizer;
 import org.bytedeco.javacpp.opencv_face.LBPHFaceRecognizer;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.processing.background.BasicBackgroundSubtract;
 import org.openimaj.image.processing.face.detection.DetectedFace;
 import org.openimaj.image.processing.face.detection.HaarCascadeDetector;
+import org.openimaj.image.processing.threshold.AbstractLocalThreshold;
+import org.openimaj.image.processing.threshold.AdaptiveLocalThresholdBernsen;
+import org.openimaj.math.geometry.point.Point2d;
 import org.openimaj.math.geometry.shape.Rectangle;
 
 import com.github.sarxos.webcam.Webcam;
@@ -54,6 +58,9 @@ public class MainFrame extends JFrame implements Runnable, WebcamPanel.Painter {
 
 	private static final Executor EXECUTOR = Executors.newSingleThreadExecutor();
 	private static final HaarCascadeDetector detector = new HaarCascadeDetector();
+	private static final BasicBackgroundSubtract<FImage> backgroundSubstract = new BasicBackgroundSubtract<>();
+	
+	private AbstractLocalThreshold thresholder;
 	
 	private static final Stroke STROKE = new BasicStroke(1.0f, 
 														BasicStroke.CAP_BUTT, 
@@ -61,6 +68,8 @@ public class MainFrame extends JFrame implements Runnable, WebcamPanel.Painter {
 														1.0f, 
 														new float[] { 1.0f }, 
 														0.0f);
+
+	private static final float MIN_FACE_WIDTH = 20;
 
 	private Webcam webcam = null;
 	private WebcamPanel.Painter painter = null;
@@ -115,20 +124,9 @@ public class MainFrame extends JFrame implements Runnable, WebcamPanel.Painter {
         for (Webcam w: Webcam.getWebcams())
         	System.out.println("["+w.getName() + "] ");
         
-        try{
-        	webcam = Webcam.getWebcamByName(LOGITECH_WEBCAM);
-        	//webcam = Webcam.getDefault();
-        }catch(Exception e){
-        	webcam = null;
-        }
+        thresholder = new AdaptiveLocalThresholdBernsen(0.1f, 3);
         
-        if (webcam == null){
-        	System.err.println("failed connecting to HD webcam, using default");
-        	webcam = Webcam.getDefault();
-        }
-        
-		webcam.setViewSize(WebcamResolution.VGA.getSize());
-		webcam.open(true);
+        createWebcam();
 
 		WebcamPanel webcamPanel = new WebcamPanel(webcam, false);
 		webcamPanel.setPreferredSize(WebcamResolution.VGA.getSize());
@@ -182,6 +180,23 @@ public class MainFrame extends JFrame implements Runnable, WebcamPanel.Painter {
 		greetingsWindow = new GreetingsWindow();
 		
 		EXECUTOR.execute(this);
+	}
+
+	private void createWebcam() {
+		try{
+        	webcam = Webcam.getWebcamByName(LOGITECH_WEBCAM);
+        	//webcam = Webcam.getDefault();
+        }catch(Exception e){
+        	webcam = null;
+        }
+        
+        if (webcam == null){
+        	System.err.println("failed connecting to HD webcam, using default");
+        	webcam = Webcam.getDefault();
+        }
+        
+		webcam.setViewSize(WebcamResolution.VGA.getSize());
+		webcam.open(true);
 	}
 
 	private void onNameSelected(){
@@ -242,17 +257,69 @@ public class MainFrame extends JFrame implements Runnable, WebcamPanel.Painter {
 		return faces != null && !faces.isEmpty();
 	}
 	
+	long backgroundTaken = 0;
+	BufferedImage webcamFrameBfImg=null;
+	FImage movementMask = null;
+
+	private int offsetX;
+
+	private int offsetY;
+
+	private FImage webcamFrameFimg;
+	
 	@Override
 	public void run() {
+		webcamFrameBfImg = webcam.getImage();
+		
+		webcamFrameFimg = ImageUtilities.createFImage(webcamFrameBfImg);
+		movementMask    = webcamFrameFimg.clone();
+		
+		backgroundSubstract.setBackground(webcamFrameFimg);
+		
+		Rectangle bounds = new Rectangle();
+		
 		while (true) {
+			
 			if (!webcam.isOpen()) 
 				return;
 			
-			faces = detector.detectFaces(ImageUtilities.createFImage(webcam.getImage()));
+			webcamFrameBfImg = webcam.getImage();
 			
-			if (!hasFaces())
-				try { Thread.sleep(100); } catch (Exception e) { throw new RuntimeException(e); } 
+			webcamFrameFimg  = ImageUtilities.createFImage(webcamFrameBfImg);
+				movementMask.internalCopy(webcamFrameFimg);
+			
+			backgroundSubstract.processImage(movementMask);
+			
+			findBounds(movementMask, bounds);
+			
+			double width = bounds.getWidth();
+			if (width < MIN_FACE_WIDTH){
+				sleep(100);
+				continue;
+			}
+			
+			FImage fragmentFimg = webcamFrameFimg.extractROI(bounds).trim();
+			
+			faces = detector.detectFaces(fragmentFimg);
+			offsetX = (int) bounds.x;
+			offsetY = (int) bounds.y;
+			
+			if (hasFaces())
+				continue;
+			
+			sleep(100);
+			
+			long now = System.currentTimeMillis();
+			if (now - backgroundTaken < 2000)
+				continue;
+			
+			backgroundTaken = now;
+			backgroundSubstract.setBackground(webcamFrameFimg);
 		}
+	}
+
+	private void sleep(int msec) {
+		try { Thread.sleep(msec); } catch (Exception e) { throw new RuntimeException(e); }
 	}
 
 	@Override
@@ -262,17 +329,31 @@ public class MainFrame extends JFrame implements Runnable, WebcamPanel.Painter {
 			return;
 		}
 		
-		try { Thread.sleep(100); } catch (Exception e) { throw new RuntimeException(e); }
+		sleep(100);
 	}
 		
 	@Override
 	public void paintImage(WebcamPanel panel, BufferedImage image, Graphics2D g2) {
 
-		if (saveCPU && (painter == null || !hasFaces()))
+		if (painter == null)
 			return;
 		
+		//if (saveCPU && !hasFaces())
+		//	return;
+		
 		painter.paintImage(panel, image, g2);
+		
+		if (movementMask == null)
+			return;
+		
+		//BufferedImage screenCut = ImageUtilities.createBufferedImage(movementMask);
+		//painter.paintImage(panel, screenCut, g2);
 
+		drawBounds(movementMask, g2);
+		
+		if (!hasFaces())
+			return;
+		
 		for(DetectedFace face : faces) {			
 			if (!recognize)
 				return;
@@ -281,8 +362,8 @@ public class MainFrame extends JFrame implements Runnable, WebcamPanel.Painter {
 
 			int dx = (int) (0.1 * bounds.width);
 			int dy = (int) (0.2 * bounds.height);
-			int x = (int) bounds.x - dx;
-			int y = (int) bounds.y - dy;
+			int x = (int) bounds.x - dx + offsetX;
+			int y = (int) bounds.y - dy + offsetY;
 			int w = (int) bounds.width + 2 * dx;
 			int h = (int) bounds.height + dy;
 
@@ -341,6 +422,41 @@ public class MainFrame extends JFrame implements Runnable, WebcamPanel.Painter {
 			
 		}
 	}
+
+	private void findBounds(FImage fimage, Rectangle bounds) {
+		int minc = fimage.width, 
+				maxc = 0, 
+				minr = fimage.height, 
+				maxr = 0;
+
+			for (int r = 0; r < fimage.height; r++) {
+				for (int c = 0; c < fimage.width; c++) {
+					if (fimage.pixels[r][c] <= 0.2f) 
+						continue;
+					if (c < minc)
+						minc = c;
+					if (c > maxc)
+						maxc = c;
+					if (r < minr)
+						minr = r;
+					if (r > maxr)
+						maxr = r;
+				}
+			}
+		bounds.setBounds(minc, minr, maxc - minc + 1, maxr - minr + 1);
+	}
+	
+	private void drawBounds(FImage fimage, Graphics2D g2) {
+		Rectangle rect = new Rectangle();
+		
+		findBounds(fimage, rect);
+		
+		g2.setStroke(STROKE);
+		g2.setColor(Color.BLUE);
+		g2.drawRect((int)rect.x, (int)rect.y, (int)rect.width, (int)rect.height);
+		
+	}
+
 
 	private void loadTrainingData(){
 
